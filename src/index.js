59f6181ebe3162b11582a9e4db742cc354b5fd37
Astros257 <48536928+Astros257@users.noreply.github.com>
2021-02-09 10:42:53 -0600

    JS expressions challenge created a react app from scratch diplayed 2 paragraph tags one says Created by YOURNAME second says Copyright CURRENTYEAR I got the current year using the new Date().getFullYear() method.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+var React = require("react");
+var ReactDom = require("react-dom");
+
+const YOURNAME = "Jose";
+const CURRENTYEAR = new Date().getFullYear();
+
+ReactDom.render(
+  <div>
+    <p>Created by {YOURNAME}</p>
+    <p>Copyright {CURRENTYEAR}</p>
+  </div>,
+  document.getElementById("root")
+);

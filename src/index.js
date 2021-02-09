1cf462f9640c51dc69413e0ed97870666e0a9e1b
Astros257 <48536928+Astros257@users.noreply.github.com>
2021-02-09 10:44:53 -0600

    changed how we import react

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
-var React = require("react");
-var ReactDom = require("react-dom");
+import React from "react";
+import ReactDom from "react-dom";
 
 const YOURNAME = "Jose";
 const CURRENTYEAR = new Date().getFullYear();
